ec79e1e31f2989ce2cf09ba8b02e9072907d1004
KonstantinMoser <konsti.m@gmx.ch>
2020-04-11 23:26:28 +0200

    started with displaytablecards

---
+++ src/components/shared/models/GameLog.js
@@ -0,0 +1,23 @@
+/**
+ * GameLog model
+ */
+class GameLog {
+    constructor(data = {}) {
+        this.transactionNr = null;
+        this.gameRound = null;
+        this.action = null;
+        this.raiseAmount = null;
+        this.playerName = null;
+        this.playerId = null;
+        this.nextPlayerName = null;
+        this.nextPlayerId = null;
+        this.playerPot = null;
+        this.roundOver = null;
+        this.gameOver = null;
+        this.amountToCall = null;
+        this.revealedCards = null;
+
+        Object.assign(this, data);
+    }
+}
+export default GameLog;
