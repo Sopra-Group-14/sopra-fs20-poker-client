12786c1dedf7d96eb519a6b35d7765da5642d301
KonstantinMoser <konsti.m@gmx.ch>
2020-04-28 16:14:22 +0200

    Problem Solving

--- src/components/gamescreen/GameScreen.js
+++ src/components/gamescreen/GameScreen.js
@@ -329,7 +329,7 @@ I already do this in the getGamelog() method
     }
 
     getImageOfCard(card){
-        let cardname = card.mySuit + card.myRank;
+            let cardname = card.mySuit + card.myRank;
         return graphicsList.find(data => data.name === cardname).src;
     }
 
