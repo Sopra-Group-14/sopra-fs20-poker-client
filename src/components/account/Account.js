e783c363adbff390ba1bce1f864562ffdadb1ec4
KonstantinMoser <konsti.m@gmx.ch>
2020-04-29 09:36:04 +0200

    Problem Solving

--- src/components/account/Account.js
+++ src/components/account/Account.js
@@ -68,8 +68,9 @@ class Account extends React.Component {
 
     async getUser() {
         try {
-            localStorage.setItem("playerId", "4");
-            const response = await api.get('https://aab96a46-4df2-44e5-abf3-1fc6f1042b6c.mock.pstmn.io/users/' + localStorage.getItem("playerId"));
+            //localStorage.setItem("playerId", "4");
+            //const response = await api.get('https://aab96a46-4df2-44e5-abf3-1fc6f1042b6c.mock.pstmn.io/users/' + localStorage.getItem("playerId"));
+            const response = await api.get('/users/' + localStorage.getItem("playerId"));
             const user = new User(response.data);
             this.handleInputChange('username', user.username);
             this.handleInputChange('balance', user.credit);
